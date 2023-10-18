61b7734ae80d99bad92a2b57db1447bf57c020eb
decker-666 <144735638+decker-666@users.noreply.github.com>
2023-10-18 21:01:13 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -572,3 +572,12 @@ setup.decrypt = function(encryptedText) {
 setup.getRandomElement = function(items) {
     return items[Math.floor(Math.random()*items.length)];
 };
+
+function shuffle(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+
+	return array;
+};
