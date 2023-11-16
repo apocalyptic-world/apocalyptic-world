e24e38bfd8a80f4657870045d3c76b9992a43b15
Māris <marislazda@gmail.com>
2023-11-16 14:50:23 +0200

    Settlement management

--- js/scripts.js
+++ js/scripts.js
@@ -677,3 +677,20 @@ setup.sexChance = function (person) {
 		return Math.max(person.relationship, person.horny, person.sub);
 	}
 };
+
+window.openTab = function(evt, name) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(name).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
