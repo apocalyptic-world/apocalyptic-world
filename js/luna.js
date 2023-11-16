3b365828174780d3583861d41336869260d8bd97
Nils Peter Andersson <144831913+NilsPeterAndersson@users.noreply.github.com>
2023-11-16 19:37:24 +0100

    Moon - fun in the forest

--- js/luna.js
+++ js/luna.js
@@ -5,8 +5,8 @@ setup.calcPhaseOfMoon = function (current = variables().gameDate) {
     const lunar_month = 29.53059;
     /* last new moon before the game starts Tue, 25 Oct 2022 10:45:21 GMT */
     const new_moon = new Date("25 Oct 2022 10:45:21 GMT");
-    const ms_diff = current.getUTCMilliseconds() - new_moon.getUTCMilliseconds();
-    const msInADay = 86400000; /* 1000*60*60*24 */
+    const ms_diff = current.getTime() - new_moon.getTime();
+    const msInADay = 86400000.0; /* 1000*60*60*24 */
     const days_diff = ms_diff/msInADay;
     return days_diff % lunar_month; /* 0..lunar_month, full in the middle, new in the ends */
 };
@@ -18,5 +18,5 @@ setup.isFullMoon = function (current = variables().gameDate) {
 
 setup.isNewMoon = function (current = variables().gameDate) {
     const moonphase = setup.calcPhaseOfMoon(current)
-    return (1 < moonphase || moonphase > 29) 
+    return (1 > moonphase || moonphase > 29) 
 }; 
