9480a36fceaf4e097001901886bbff9e4cafb576
Nils Peter Andersson <144831913+NilsPeterAndersson@users.noreply.github.com>
2023-11-18 19:33:52 +0100

    Start of modding system
    Here is my version of a modding system.
    
    A settings page under settlement management so the user can choose what option/mod to use.
    
    settings.mods for static data & $game.mods  for the flags
    
    Working for now:
    - A hunter without bow will go back to cut woood.
    
    Hope U like it.

---
+++ js/mods.js
@@ -0,0 +1,16 @@
+/* game modification support */
+setup.mods = {
+    auto_axe: {
+        name: 'Axe',
+        description: 'An axe for every forest worker, while stocks last.',
+    },
+    auto_bow: {
+        name: 'Bow',
+        description: 'An bow for every hunter, while stocks last.',
+    },
+    auto_hunt: {
+        name: 'Hunter',
+        description: 'A hunter without a bow hunts berries and wood instead.',
+    }
+};
+
