4c071b17ceb9023825cdaa9930f73e2a392b9058
Peter Andersson <144831913+NilsPeterAndersson@users.noreply.github.com>
2023-11-20 22:26:50 +0100

    Automatization - Hunting (#60)

--- js/npc/inventory.js
+++ js/npc/inventory.js
@@ -37,4 +37,13 @@ setup.cabinInvHas = function (item, count=1) {
     return setup.cabinInvCount(item) >= count;
 }
 
+setup.cabinInvDrop = function (item, count=1) {
+    const storeAvailable = variables().game.location.shop ?? false;
+    const backpack = variables().backpack;
+    if (storeAvailable) {
+        const storage = variables().storage;
+        storage.transfer(backpack, item, count);
+    } 
+    backpack.drop(item, count);
+}
 /* && */
