4bf786626291e9a2858701b5da835dd63ba5eefe
Māris <marislazda@gmail.com>
2023-12-02 23:01:34 +0200

    Standalone mods

---
+++ js/mods.js
@@ -0,0 +1,12 @@
+setup.mods = {
+    anita: {
+        name: 'Anita',
+        description: 'Pornstart Anita mod pack',
+        download: 'http://google.com'
+    },
+    gayExtended: {
+        name: 'Gay extended',
+        description: 'More gay images',
+        download: 'http://google.com'
+    }
+};
