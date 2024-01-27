b0342d6240c63bd8835405949c70e65884c1f6a7
Māris <marislazda@gmail.com>
2024-01-27 23:17:50 +0200

    Updates

--- js/events.js
+++ js/events.js
@@ -2,15 +2,9 @@ setup.neganQuest2HaveGirls = function()
 {
     var girlIds = [];
     for (var _companionId in (variables().player.companions ?? {})) {
-        var _npcType = _companionId.split(':')[0];
-        var _npcId = _companionId.split(':')[1];
-        if (_npcType === 'guest') {
-            var _npc = variables().guests[_npcId];
-        } else {
-            var _npc = variables().slaves[_npcId]
-        }
+        var _npc = setup.companionGet(_companionId);
         if (!_npc.gender && _npc.beauty >= 80 && _npc.hair === 'blonde') {
-            girlIds.push(_npcId)
+            girlIds.push(_companionId)
         }
     }
 
