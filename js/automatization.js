4cbdfe23578d5fcefa29ec4761e01f7c9b676efb
dcosmic1 <161456016+dcosmic1@users.noreply.github.com>
2024-03-01 21:50:36 +0100

    Phrasing adjustments and typo removal (#135)

--- js/automatization.js
+++ js/automatization.js
@@ -1,16 +1,16 @@
 /* game modification support */
 setup.automatization = {
     rule: {
-/*
-        add: {
-            name: 'Add',
-            description: 'Add job tools to settlers, while stocks last. {}'
-        },
-        subtract: {
-            name: 'Subtract',
-            description: 'Add and remove job tools on settlers {}'
-        },
-*/
+        /*
+                add: {
+                    name: 'Add',
+                    description: 'Add job tools to settlers, while stocks last. {}'
+                },
+                subtract: {
+                    name: 'Subtract',
+                    description: 'Add and remove job tools on settlers {}'
+                },
+        */
         dup: {
             name: 'No duplicates',
             description: 'Removes duplicates from settler inventory. 2 bows is not better than 1 bow.'
@@ -25,48 +25,44 @@ setup.automatization = {
             description: 'Hunter with no bow works as a woodcutter; Might get an axe instead.'
         },
         streetworker: {
-            name: 'Streetworkers need rest',
+            name: 'Street workers need rest',
             description: 'Very unhappy, sad street workers takes a day off by themselves'
         },
 
         buttplug: {
-            name: 'Buttplug rotation',
-            description: 'Settlers trade buttplugs between them as needed, thoroughly cleaned of course ;-)'
+            name: 'Butt plug rotation',
+            description: 'Settlers trade butt plugs between them as needed, thoroughly cleaned of course ;-)'
         },
         condom: {
             name: 'Condom rotation',
-            description: 'Gives condoms to girls who need it and removes from other. Only streetworkers gets condoms for now.'
+            description: 'Gives condoms to girls who need it and removes from other. Only street workers gets condoms for now.'
         },
         dumbbell: {
             name: 'Dumbbell rotation',
-            description: 'Gives dumbbbels to low strength settlers (below 30) and removes from other.'
+            description: 'Gives dumbbells to low strength settlers (below 30) and removes from other.'
         },
 
-/*
-        grouptalk: {
-            name: 'Group talk',
-            description: 'Talk with (almost) all persons in a group. Saves clicks only! (WIP, not in game yet)'
-        },
-*/
+        /*
+                grouptalk: {
+                    name: 'Group talk',
+                    description: 'Talk with (almost) all persons in a group. Saves clicks only! (WIP, not in game yet)'
+                },
+        */
     },
 
     job_tools: {
-        companion:  ['knife', 'gas_mask'],
-        forest:     ['axe'],
-        garden:     [],
-        guard:      ['knife'],
-        hunter:     ['bow'],
-        kitchen:    [],
-        maid:       [],
-        mistress:   [],
-        none:       [],
+        companion: ['knife', 'gas_mask'],
+        forest: ['axe'],
+        garden: [],
+        guard: ['knife'],
+        hunter: ['bow'],
+        kitchen: [],
+        maid: [],
+        mistress: [],
+        none: [],
         scavenging: ['knife'],
-        shop:       [],
-        streets:    ['knife'],
-        mc:         ['knife', 'bow', 'gas_mask', 'axe'],
+        shop: [],
+        streets: ['knife'],
+        mc: ['knife', 'bow', 'gas_mask', 'axe'],
     }
 };
-
-
-
-
