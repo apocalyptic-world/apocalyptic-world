2c3c6bd6135a17ed7eae2ef2e6cef69f6706c514
dcosmic1 <161456016+dcosmic1@users.noreply.github.com>
2024-03-02 21:58:16 +0100

    First few lines added for generic testing :)  (#138)

--- views/npc/fuck/mc/Texts/female/anal_fingering.js
+++ views/npc/fuck/mc/Texts/female/anal_fingering.js
@@ -1,6 +1,11 @@
 setup.actionText ??= {};
 setup.actionText.anal_fingering = {
     texts: [
-        "Your fingers dig into %name%'s asshole, you feel her anus pulsing around your finger!"
+        "Your fingers dig into %name%'s asshole, you feel her anus pulsing around your finger!",
+        "You tease %name%'s asshole with your fingertips, watching her squirm with desire.",
+        "%name%'s body tenses with pleasure as you stimulate her asshole with your probing fingers.",
+        "You explore %name%'s body with your fingers, fucking her with a finger inside her tight ass.",
+        "%name% moans with pleasure as you stimulate her anus, slowly inserting a finger into her tight ass.",
+        "You stick a finger inside %name% tight asshole, preparing her for big things to come."
     ]
 }
--- views/npc/fuck/mc/Texts/female/handjob.js
+++ views/npc/fuck/mc/Texts/female/handjob.js
@@ -1,6 +1,13 @@
 setup.actionText ??= {};
 setup.actionText.handjob = {
     texts: [
-        "%name% gives you a nice handjob, a good start!"
+        "%name% gives you a nice hand job, a good start!",
+        "With a skilled grip, %name% starts stroking you off, teasing you mercilessly as you twitch with pleasure.",
+        "You moan softly as %name% takes hold of your cock, her touch sending jolts of pleasure through your body.",
+        "You feel your excitement building as %name% gives you a delicious hand job.",
+        "A slow and steady hand job from %name% builds your arousal, leaving you wanting more.",
+        "Warmth spreads through your body as %name% wraps her hand around you.",
+        "Your cock throbs with anticipation as %name% begins the action with a sensual hand job.",
+        "With a seductive glint in their eye, %name% treats you to a slow and satisfying hand job."
     ]
 }
