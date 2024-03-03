9bd550eb456cfd71144fc02c380db501cd0ef883
Nils Peter Andersson <144831913+NilsPeterAndersson@users.noreply.github.com>
2024-03-03 21:22:59 +0100

    Extend Explore event
    The Dying Elderly Survivor

--- js/npc/companion.js
+++ js/npc/companion.js
@@ -6,4 +6,38 @@ setup.companionGet = function(key)
     }
 
     return variables().slaves[splitKey[1]];
-}
+};
+
+/**
+ * "namespace" for companions-list help functions
+ */
+setup.companions = {
+    /**
+     * All compannions who has a certain skills
+     * @param {companion} companions
+     * @param {string} skill                   
+     * @returns [npc]  npcs                      
+     */
+    getBySkill: function(companions = variables().player.companions, skill = '') {
+        const skilledCompanions = [];
+        for(const companionKey in companions) {
+            const splitKey = companionKey.split(':');
+            const list = splitKey[0] + 's'; // guests or slaves
+            const index = splitKey[1];
+            const npc = variables()[list][index];
+            if ((npc.skills ?? []).includes('doctor')) {
+                skilledCompanions.push(npc);
+            }
+        }
+        return skilledCompanions;
+    },
+
+    /**
+     * all doctors
+     * @param {companions} companions 
+     * @returns  [npc]  npcs
+     */
+    getDoctors: function(companions = variables().player.companions) {
+        return setup.companions.getBySkill(companions, 'doctor');
+    },
+};
