d0ae53683c1c5193e29143da74ce0c36402e32aa
decker-666 <144735638+decker-666@users.noreply.github.com>
2024-03-05 20:24:10 +0100

    ask to fuck lesbian threesome (#142)

--- js/scripts.js
+++ js/scripts.js
@@ -690,6 +690,15 @@ function shuffle(array) {
 	return array;
 };
 
+setup.shuffling = function(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+
+	return array;
+};
+
 /* convert map.keys() to a regular list since sugarcube is stupid when it comess to Map */
 setup.mapKeys2list = function(map) {
     const list = [];
