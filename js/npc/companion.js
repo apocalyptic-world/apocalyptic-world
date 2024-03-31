6a63402cacc5a82eaf102276f8e4fa0e3028a00b
Nils Peter Andersson <144831913+NilsPeterAndersson@users.noreply.github.com>
2024-03-31 22:01:13 +0200

    minor tweaks - sanctuary infiltration
    companion.js - functionality
    Farm -
    Journal - typo

--- js/npc/companion.js
+++ js/npc/companion.js
@@ -13,7 +13,7 @@ setup.companionGet = function(key)
  */
 setup.companions = {
     /**
-     * All companions who has a certain skills
+     * All companions who has a certain skill
      * @param {companion} companions
      * @param {string} skill                   
      * @returns [npc]  npcs                      
@@ -24,8 +24,8 @@ setup.companions = {
             const splitKey = companionKey.split(':');
             const list = splitKey[0] + 's'; // guests or slaves
             const index = splitKey[1];
-            const npc = variables()[list][index];
-            if ((npc.skills ?? []).includes('doctor')) {
+            const npc = variables()[list][index] ?? {};
+            if ((npc.skills ?? []).includes(skill)) {
                 skilledCompanions.push(npc);
             }
         }
@@ -34,7 +34,7 @@ setup.companions = {
 
     /**
      * all doctors
-     * @param {companions} companions 
+     * @param {companion} companions 
      * @returns  [npc]  npcs
      */
     getDoctors: function(companions = variables().player.companions) {
@@ -43,10 +43,51 @@ setup.companions = {
 
     /**
      * count companions
-     * @param {companions}   companions 
+     * @param {companion}   companions 
      * @returns {int} number of companions
      */
     count: function(companions = variables().player.companions) {
         return Object.keys(companions).length;
     },
+
+    /**
+     * Check if the companions in the list is valid or not
+     * @param {companion} companions 
+     * @returns {boolean}      valid list or not
+     */
+    check: function(companions = variables().player.companions) {
+        for(const companionKey in companions) {
+            const splitKey = companionKey.split(':');
+            const list = splitKey[0] + 's'; // guests or slaves
+            const index = splitKey[1];
+            const npc = variables()[list][index];
+            if (typeof npc === 'undefined') {
+                return false;
+            }
+        }
+        return true;
+    },
+
+    /**
+     * check if we have the right number of companions with right strength
+     * @param {companion} companions 
+     * @param {int}       amount        number of companions
+     * @param {int}       minstrength   minimium strength needed
+     * @returns {boolean}
+     */
+    sanctuary_infiltration: function(companions = variables().player.companions, amount = 4, minstrength = 40) {
+        if(setup.companions.count(companions) !== amount ) {
+            return false;
+        }
+        for(const companionKey in companions) {
+            const splitKey = companionKey.split(':');
+            const list = splitKey[0] + 's'; // guests or slaves
+            const index = splitKey[1];
+            const npc = variables()[list][index] ?? {};
+            if ((npc.strength ?? 0) < minstrength) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
