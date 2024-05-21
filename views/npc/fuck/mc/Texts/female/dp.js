f077e11e948010463a156e822e654e6b8fb713ee
Māris <marislazda@gmail.com>
2024-05-21 20:55:38 +0300

    Fixes

--- views/npc/fuck/mc/Texts/female/dp.js
+++ views/npc/fuck/mc/Texts/female/dp.js
@@ -2,5 +2,12 @@ setup.actionText ??= {};
 setup.actionText.dp = {
     texts: [
         "You grab the %name%'s head with your hands and drive your hard dick deep into her throat.<br />%name% coughs when her gag reflex kicks in."
-    ]
+    ],
+    location: {
+        gloryhole: {
+            texts: [
+                "You feel that she fully takes your dick into her mouth as deep as she can.<br />%name% coughs when her gag reflex kicks in" 
+            ]
+        }
+    }
 }
