e8836b713866fba1a675cdb6e6d9d05c43a98e2b
Peter Andersson <144831913+NilsPeterAndersson@users.noreply.github.com>
2024-06-12 22:38:51 +0200

    Fixes management + help (#163)

--- js/automatization.js
+++ js/automatization.js
@@ -13,33 +13,43 @@ setup.automatization = {
         */
         dup: {
             name: 'No duplicates',
-            description: 'Removes duplicates from settler inventory. 2 bows is not better than 1 bow.'
+            description: 'Removes duplicates from settler inventory. 2 bows is not better than 1 bow.',
+            help: 'When on:<ul>' +
+                '<li>Prevents you from giving a settler duplicate items</li>' + 
+                '<li>Morning check removes duplicates</li></ul>When off<ul>' +
+                '<li>Morning check - other rules may remove duplicates relevant to the rule</li></ul>'
         },
         job: {
             name: 'Job change',
-            description: 'When changing job (companions, assign to ...) give the settler a tool box for the new job.'
-        },
+            description: 'When changing job (companions, assign to ...) give the settler a tool box for the new job and removes when unassigned.',
+            help: 'When on: Toolbox with mandatory and optional tools for the job given when you assign to job and updated in the morning check<br/>When off: You have to do it yourselves!'
+        },      
 
         hunter: {
             name: 'Hunter with no bow',
-            description: 'Hunter with no bow works as a woodcutter; Might get an axe instead.'
+            description: 'Hunter with no bow works as a woodcutter; Might get an axe instead.',
+            help: 'When off: Hunter with no bow does nothing'
         },
         streetworker: {
             name: 'Sex workers need rest',
-            description: 'Very unhappy, sad street & nightclub workers takes a day off by themselves'
+            description: 'Very unhappy, sad street & nightclub workers takes a day off by themselves',
+            help: ''
         },
 
         buttplug: {
             name: 'Butt plug rotation',
-            description: 'Settlers trade butt plugs between them as needed, thoroughly cleaned of course ;-)'
+            description: 'Settlers trade butt plugs between them as needed, thoroughly cleaned of course ;-)',
+            help: ''
         },
         condom: {
             name: 'Condom rotation',
-            description: 'Gives condoms to girls who need it and removes from other. Only street workers gets condoms for now.'
+            description: 'Gives condoms to girls who need it and removes from other. Only street workers gets condoms for now.',
+            help: ''
         },
         dumbbell: {
             name: 'Dumbbell rotation',
-            description: 'Gives dumbbells to low strength settlers (below 30) and removes from other.'
+            description: 'Gives dumbbells to low strength settlers (below 30) and removes from other.',
+            help: ''
         },
 
         /*
@@ -50,6 +60,10 @@ setup.automatization = {
         */
     },
 
+    /**
+     * Toolboxes for different jobs/assignemnt. Can be set here or as a tag
+     * tool_<job> in "Inventory items.tw"
+     */
     job_tools: {
         companion: ['knife', 'gas_mask', 'body_armor'],
         companion_slave: ['gas_mask'],
@@ -64,7 +78,20 @@ setup.automatization = {
         none: [],
         scavenging: ['knife', 'body_armor'],
         shop: [],
+        teacher: [],
         streets: ['knife'],
         mc: ['knife', 'bow', 'gas_mask', 'axe', 'body_armor'],
-    }
+    },
+
+    _initDone: false,
+    init: function() {
+        if(this._initDone) {
+            return;
+        }
+
+        // WORK 
+
+        this._initDone = true;
+        return;
+    },
 };
