5be85b85dbef95e02fc2bbadd64b4d503cd80e21
Māris <marislazda@gmail.com>
2024-08-20 17:12:02 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -906,3 +906,36 @@ setup.extendPack = function(obj1, obj2) {
 
     return merged;
 }
+
+setup.mergeObjects = function(obj1, obj2, parentPath = '', packName = '') {
+    const merged = {};
+
+    for (const key in obj1) {
+        if (obj1.hasOwnProperty(key)) {
+            if (typeof obj1[key] === 'object' && !Array.isArray(obj1[key])) {
+                merged[key] = setup.mergeObjects(obj1[key], obj2[key] || {}, `${parentPath}/${key}`, packName);
+            } else {
+                merged[key] = obj1[key];
+            }
+        }
+    }
+
+    for (const key in obj2) {
+        if (obj2.hasOwnProperty(key)) {
+            if (typeof obj2[key] === 'object' && !Array.isArray(obj2[key])) {
+                if (!merged.hasOwnProperty(key)) {
+                    merged[key] = setup.mergeObjects({}, obj2[key], `${parentPath}/${key}`, packName);
+                }
+            } else if (Array.isArray(obj2[key])) {
+                merged[key] = merged[key] || [];
+                obj2[key].forEach(value => {
+                    merged[key].push(`${packName}${parentPath}/${key}/${value}`);
+                });
+            } else {
+                merged[key] = obj2[key];
+            }
+        }
+    }
+
+    return merged;
+}
