cfa5ad9f70556c4f389ca67246d8aa46967c4a06
Dan Decker <decker@netcourrier.com>
2024-10-25 22:39:11 +0200

    threesome mff gamma

---
+++ views/npc/fuck/npc/Texts/threesome_mff/threesome_mff_texts.js
@@ -0,0 +1,78 @@
+setup.actionText ??= {};
+setup.actionText.threesome_mff = {
+    kissing: [
+        "%females% are kissing %male% and each other sweetly."
+    ],
+    handjob: [
+        "%females% are giving %male% a nice handjob, a good start!"
+    ],
+    footjob: [
+        "%male% is pleasuring himself with %females%'s feet."
+    ],
+    titjob: [
+        "%females% are giving %male% a nice titjob!"
+    ],	
+    bj: [
+        "%females% suck %male%'s hard dick with their warm mounths and agile tongues!"
+    ], 
+    dp: [
+        "%male% grabbed %females%'s heads with his hands and is driving his hard dick deep into their throats."
+    ],
+    nipple_lick: [
+        "%male% is licking %females%'s nipples, while squeezing their tits!"
+    ],
+    pussy_lick: [
+        "%male% is licking %females%'s clits, they seem to like it a lot!"
+    ],
+    pussy_fingering: [
+        "%male% fingers %females%'s pussies."
+    ],
+    pussy: [
+        "%male% fucks %females%'s wet pussies"
+    ],
+    anal_fingering: [
+        "%male%'s fingers dig deep in %females%'s assholes!"
+    ],
+    anal: [
+        "%females% moan softly as %male% fucks their asses."
+    ],
+    anal_reverse: [
+        "%male% moans softly as %females% fuck his ass with a strapon."
+    ],
+    anal_reverse_cum: [
+        "%male% starts to cum while %female0%'s strapon is still in his ass!"
+    ],
+    handjob_cum: [
+        "%male% jerks off with %females%'s hands tightly gripped around his cock."
+    ],
+    footjob_cum: [
+        "%male% covers %females%'s feet with his seed."
+    ],
+    cum_on_tits: [
+        "%male%'s cum covers %females%'s tits."
+    ],
+    cum_in_pussy: [
+        "%male% continues to fuck %female0% non-stop and pumps her pussy full of his seed."
+    ],
+    cum_on_stomach: [
+        "%male% pulls out and finishes on %females%'s stomachs."
+    ],
+    cum_in_ass: [
+        "With a groan, %male% cums in %female0% ass."
+    ],
+    cum_on_back: [
+        "With a groan, %male% cums on %females%'s back sides."
+    ],
+    cum_on_face: [
+        "%male% cums all over %females%'s faces."
+    ],
+    cum_in_mouth: [
+        "%male% fills %females%'s mouths with his cum."
+    ],
+    cum_throat: [
+        "%male% grabs %female0% head and hold her as he shoves his dick deep into her throat and starts cumming."
+    ],
+    squirt_lick: [
+        "%female0% screams of pleasure and starts squirting while %male% continues licking her pussy."
+    ]
+};
