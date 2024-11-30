d0ffbe12e2f342c27bedddbbb0785ba1c349b61a
decker-666 <144735638+decker-666@users.noreply.github.com>
2024-11-30 23:09:01 +0100

    mc_threesome_ff (#196)

--- views/npc/fuck/npc/Texts/female_on_female/female_on_female_texts.js
+++ views/npc/fuck/npc/Texts/female_on_female/female_on_female_texts.js
@@ -7,7 +7,7 @@ setup.actionText.female_on_female = {
         "%names% sensually caress, kiss and suck each other's feet."
     ],
     spanking: [
-        "%name1% spanks %name2%, saying things about being a naughty girl."
+        "%name% spanks %name2%, saying things about being a naughty girl."
     ],
     sn_tits: [
         "%names% suck each other's breasts, you can hear suction sounds."
@@ -16,46 +16,46 @@ setup.actionText.female_on_female = {
         "%names% frantically rub each other's clits, making sweet moaning sounds."
     ],
     rimjob: [
-        "%name1% explores %name2%'s asshole with her tongue, they both seem delighted."
+        "%name% explores %name2%'s asshole with her tongue, they both seem delighted."
     ],
     facesitting: [
-        "%name1% gets her pussy properly eaten by %name2%, you can hear heavy breathing."
+        "%name% gets her pussy properly eaten by %name2%, you can hear heavy breathing."
     ],
     strapon_bj: [
-        "%name1% takes a strapon dildo in her mouth, making it as wet as possible."
+        "%name% takes a strapon dildo in her mouth, making it as wet as possible."
     ],
     dd_deepthroat: [
-        "%name1% and %name2% take a double dildo deep down in their throats, making gagging sounds."
+        "%name% and %name2% take a double dildo deep down in their throats, making gagging sounds."
     ],
     scissoring: [
-        "%name1% and %name2% rub their clits one against each other's, you can hear heavy breathing."
+        "%name% and %name2% rub their clits one against each other's, you can hear heavy breathing."
     ],
     strapon_pussy: [
-        "%name1% takes a strapon dildo in her pussy, you can hear her calling out in pleasure."
+        "%name% takes a strapon dildo in her pussy, you can hear her calling out in pleasure."
     ],
     dd_pussy: [
-        "%name1% and %name2% motion back an forth connected by a double dildo in their pussies."
+        "%name% and %name2% motion back an forth connected by a double dildo in their pussies."
     ],
     fisting_pussy: [
-        "%name1% takes a whole fist in her pussy, calling out %name2%'s name."
+        "%name% takes a whole fist in her pussy, calling out %name2%'s name."
     ],
     sn_pussy: [
         "%names% lick each other's pussies in perfect symmetry."
     ],
     strapon_anal: [
-        "%name1% takes a strapon dildo in the ass, you hear a mix of moans and cries."
+        "%name% takes a strapon dildo in the ass, you hear a mix of moans and cries."
     ],
     strapon_double: [
-        "%name1% starts screaming as she takes a strapon dildo in the pussy and another in the ass."
+        "%name% starts screaming as she takes a strapon dildo in the pussy and another in the ass."
     ],
     dd_anal: [
         "%names% play with a double dildo inserting it in their asses."
     ],
     fisting_anal: [
-        "%name1% takes a whole fist in her ass, you can read the pain and pleasure on her face."
+        "%name% takes a whole fist in her ass, you can read the pain and pleasure on her face."
     ],
     orgasm: [
-        "%name1% makes a sharp scream of pleasure as she gets an orgasm."
+        "%name% makes a sharp scream of pleasure as she gets an orgasm."
     ],
     squirting: [
         "%names% orgasm in turn, one of them squirts her juices out of intense pleasure."
