c93eebe44a65cd6b57a4ee46aabd055efa6fe52b
Dan Decker <decker@netcourrier.com>
2024-11-30 15:46:36 +0100

    name number

--- views/npc/fuck/mc/Texts/female/threesome_f_texts.js
+++ views/npc/fuck/mc/Texts/female/threesome_f_texts.js
@@ -1,34 +1,34 @@
 setup.actionText ??= {};
 setup.actionText.female = {
     threesome_f_kissing: [
-        "%name% and %name1% are kissing you and each other sweetly."
+        "%name% and %name2% are kissing you and each other sweetly."
     ],
     threesome_f_handjob: [
-        "%name% and %name1% give you a nice handjob, a good start!"
+        "%name% and %name2% give you a nice handjob, a good start!"
     ],
     threesome_f_footjob: [
-        "You pleasure yourself with %name% and %name1%'s feet."
+        "You pleasure yourself with %name% and %name2%'s feet."
     ],
     threesome_f_titjob: [
-        "%name% and %name1% give you a nice titjob!"
+        "%name% and %name2% give you a nice titjob!"
     ],	
     threesome_f_bj: [
-        "%name% and %name1% suck your hard dick in turn with their warm mounths and agile tongues!"
+        "%name% and %name2% suck your hard dick in turn with their warm mounths and agile tongues!"
     ], 
     threesome_f_dp: [
         "You grab %name%'s head with your hands and drive your hard dick deep into her throats."
     ],
     threesome_f_nipple_lick: [
-        "You and %name1% lick %name%'s nipples, and squeeze her tits hard!"
+        "You and %name2% lick %name%'s nipples, and squeeze her tits hard!"
     ],
     threesome_f_sn: [
-        "You lick %name%'s pussy, while she licks %name1%'s pussy, who is sucking your dick!"
+        "You lick %name%'s pussy, while she licks %name2%'s pussy, who is sucking your dick!"
     ],
     threesome_f_pussy_lick: [
         "You lick %name%'s clits, she seems to like it a lot!"
     ],
     threesome_f_pussy_fingering: [
-        "You finger %name% and %name1%'s pussies, they are getting very wet!"
+        "You finger %name% and %name2%'s pussies, they are getting very wet!"
     ],
     threesome_f_pussy: [
         "You fuck %name%'s tight pussy"
@@ -49,19 +49,19 @@ setup.actionText.female = {
         "You start to cum while %name%'s strapon is still in your ass!"
     ],
     threesome_f_handjob_cum: [
-        "You jerk off with %name% and %name1%'s hands tightly gripped around your cock."
+        "You jerk off with %name% and %name2%'s hands tightly gripped around your cock."
     ],
     threesome_f_footjob_cum: [
-        "You cover %name% and %name1%'s feet with your seed."
+        "You cover %name% and %name2%'s feet with your seed."
     ],
     threesome_f_cum_on_tits: [
-        "Your cum covers %name% and %name1%'s breasts."
+        "Your cum covers %name% and %name2%'s breasts."
     ],
     threesome_f_cum_in_pussy: [
         "You continue to fuck %name% non-stop and pump her pussy full of your seed."
     ],
     threesome_f_cum_on_stomach: [
-        "You pull out and finish on %name% and %name1%'s stomachs."
+        "You pull out and finish on %name% and %name2%'s stomachs."
     ],
     threesome_f_cum_in_ass: [
         "With a groan, you cum in %name%'s ass."
@@ -70,12 +70,12 @@ setup.actionText.female = {
         "With a groan, you cum on %name%'s ass."
     ],
     threesome_f_cum_on_back: [
-        "With a groan, you cum on %name% and %name1%'s back sides."
+        "With a groan, you cum on %name% and %name2%'s back sides."
     ],
     threesome_f_cum_on_face: [
-        "You cum all over %name% and %name1%'s faces."
+        "You cum all over %name% and %name2%'s faces."
     ],
     threesome_f_cum_in_mouth: [
-        "You fill %name% and %name1%'s mouths with your cum."
+        "You fill %name% and %name2%'s mouths with your cum."
     ]
 };
