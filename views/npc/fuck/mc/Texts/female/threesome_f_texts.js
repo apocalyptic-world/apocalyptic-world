57e3087fdaefedec23557533350b2543397ebf63
Dan Decker <decker@netcourrier.com>
2024-12-10 19:36:50 +0100

    more mff

--- views/npc/fuck/mc/Texts/female/threesome_f_texts.js
+++ views/npc/fuck/mc/Texts/female/threesome_f_texts.js
@@ -33,6 +33,9 @@ setup.actionText.female = {
     threesome_f_pussy: [
         "You fuck %name%'s tight pussy"
     ],
+    threesome_f_pussy_pussy_lick: [
+        "You fuck %name%'s tight pussy, while licking %name2%'s clit."
+    ],
     threesome_f_anal_fingering: [
         "Your fingers dig deep into %name%'s tight asshole!"
     ],
@@ -45,10 +48,58 @@ setup.actionText.female = {
     threesome_f_anal_reverse: [
         "You moan softly as %name% fucks your ass with a strapon."
     ],
+    threesome_f_bj_anal_reverse: [
+        "%name% gives you a blowjob while %name2% fucks your ass with her strapon."
+    ],
+    threesome_f_bj_reverse_anal_reverse: [
+        "%name% fucks your mouth while %name2% fucks your ass with their strapons."
+    ],
+    threesome_f_handjob_anal_reverse: [
+        "%name% gives you a handjob while %name2% fucks your ass with her strapon."
+    ],
+    threesome_f_nipple_lick_anal_reverse: [
+        "You lick %name%'s nipple while %name2% fucks your ass with her strapon."
+    ],
+    threesome_f_pussy_anal_reverse: [
+        "You fuck %name%'s pussy while %name2% fucks your ass with her strapon."
+    ],
+    threesome_f_pussy_lick_anal_reverse: [
+        "You lick %name%'s pussy while %name2% fucks your ass with her strapon."
+    ],
+    threesome_f_sn_anal_reverse: [
+        "You lick %name%'s pussy while she gives you a blowjob and %name2% fucks your ass with her strapon."
+    ],
     threesome_f_anal_reverse_cum: [
         "You start to cum while %name%'s strapon is still in your ass!"
     ],
-    threesome_f_handjob_cum: [
+    threesome_f_anal_a_pussy_lick: [
+        "You fuck %name% in the ass while she licks %name2%'s pussy!"
+    ],
+    threesome_f_anal_b_bj: [
+        "You fuck %name% in the ass while she sucks %name2%'s strapon!"
+    ],
+    threesome_f_anal_b_pussy: [
+        "You fuck %name% in the ass while %name2% fucks her with a strapon!"
+    ],
+    threesome_f_bj_b_anal: [
+        "%name% sucks your cock while %name2% fucks her in the ass with a strapon!"
+    ],
+    threesome_f_bj_b_pussy: [
+        "%name% sucks your cock while %name2% fucks her with a strapon!"
+    ],
+    threesome_f_pussy_a_pussy: [
+        "You fuck %name% while she fucks %name2% with a strapon!"
+    ],
+    threesome_f_pussy_a_pussy_lick: [
+        "You fuck %name% while she licks %name2%'s pussy!"
+    ],
+    threesome_f_pussy_a_sn: [
+        "You fuck %name% while she and %name2% lick each others pussy!"
+    ],
+    threesome_f_pussy_b_anal: [
+        "You fuck %name% while %name2% fucks her in the ass with a strapon!"
+    ],
+    threesome_f_cum: [
         "You jerk off with %name% and %name2%'s hands tightly gripped around your cock."
     ],
     threesome_f_footjob_cum: [
