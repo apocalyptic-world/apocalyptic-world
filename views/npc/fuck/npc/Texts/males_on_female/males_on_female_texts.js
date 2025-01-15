64fc86d26ecc961d895cfdbbf3172336d65ff0e3
Dan Decker <decker@netcourrier.com>
2025-01-15 18:40:38 +0100

    fmm event and misc

---
+++ views/npc/fuck/npc/Texts/males_on_female/males_on_female_texts.js
@@ -0,0 +1,78 @@
+setup.actionText ??= {};
+setup.actionText.males_on_female = {
+    kissing: [
+        "%male% and %male2% are kissing %female% with passion."
+    ],
+    handjob: [
+        "%female% is giving %male% and %male2% a nice handjob, a good start!"
+    ],
+    titjob: [
+        "%male% and %male2%'s dicks rub %female%'s tits, all slippery from their pre-cum."
+    ],	
+    bj: [
+        "%female% sucks %male% and %male2%'s hard dicks with her warm mounth and agile tongue!"
+    ],
+    bj_b_pussy_lick: [
+        "%female% sucks %male% dick, while %male2% is licking her wet pussy!"
+    ],
+    nipple_lick: [
+        "%male% and %male2% are licking %female%'s nipples, while squeezing her tits!"
+    ],
+    pussy_fingering: [
+        "%male% is fingering %female%'s pussy."
+    ],
+    pussy_lick: [
+        "%male% is licking %female%'s clit, she seems to like it a lot!"
+    ],
+    pussy: [
+        "%female% starts moaning as %male% fucks her"
+    ],
+    pussy_b_handjob: [
+        "%female% moans softly as %male% fucks her pussy, while she gives a handjob to %male2%."
+    ],
+    pussy_b_bj: [
+        "%female% moans softly as %male% fucks her pussy, while she gives a blowjob to %male2%."
+    ],
+    pussy_b_anal: [
+        "%female% moans softly as %male% fucks her pussy, while %male2% fucks her ass."
+    ],
+    pussy_b_pussy: [
+        "%female% screams as %male% and %male2% both fuck her pussy at the same time."
+    ],
+    anal: [
+        "%female% moans softly as %male% fucks her ass, looks like she is enjoying it."
+    ],
+    anal_b_handjob: [
+        "%female% moans softly as %male% fucks her ass, while she gives a handjob to %male2%."
+    ],
+    anal_b_bj: [
+        "%female% moans softly as %male% fucks her ass, while she gives a blowjob to %male2%."
+    ],
+    anal_b_pussy: [
+        "%female% moans softly as %male% fucks her ass, while %male2% fucks her pussy."
+    ],
+    anal_b_anal: [
+        "%female% screams as %male% and %male2% both fuck her ass at the same time."
+    ],
+    anal_fisting: [
+        "%female% screams sharply as %male% fists her ass."
+    ],
+    cum_in_ass: [
+        "With a groan, %male% cums in %female%'s ass."
+    ],
+    cum_in_mouth: [
+        "%male% and %male2% fill %female%'s mouth with their cum."
+    ],
+    cum_on_face: [
+        "%male% and %male2% cum all over %female%'s face."
+    ],
+    cum_on_stomach: [
+        "%male% and %male2% cum all over %female%'s stomach."
+    ],
+    cum_on_tits: [
+        "%male% and %male2%'s cum covers %female%'s tits."
+    ],
+    double_cum: [
+        "%male% fills %female%'s pussy with cum while %male2% does the same to her ass."
+    ]
+};
