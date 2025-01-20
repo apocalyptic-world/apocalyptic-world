5d2b27cdb4793a0077b2602ef3ca67cf77dc98b1
Dan Decker <decker@netcourrier.com>
2025-01-20 22:01:03 +0100

    pussy fisting + trans orgasm + dream options

---
+++ views/npc/fuck/mc/Texts/female/anal_fisting.js
@@ -0,0 +1,11 @@
+setup.actionText ??= {};
+setup.actionText.anal_fisting = {
+    texts: [
+        "Your ram your fist deep into %name%'s asshole, you feel her anus pulsing around you!",
+        "You stuff %name%'s asshole with your hand, watching her squirm with pain and pleasure.",
+        "%name%'s body tenses with pleasure and pain as you fist her asshole.",
+        "You explore %name%'s body with your fist, fucking her with all five fingers inside her tight ass.",
+        "%name% moans with pleasure and pain as you destroy her anus, slowly inserting all your fingers into her tight ass.",
+        "You stick your hand inside %name% tight asshole."
+    ]
+}
