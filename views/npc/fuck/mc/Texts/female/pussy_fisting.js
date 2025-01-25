dd92d6d632393d6ef1492c10237260d4f776d2f7
decker-666 <144735638+decker-666@users.noreply.github.com>
2025-01-25 20:06:01 +0100

    pussy fisting + trans orgasm + dream options (#206)

---
+++ views/npc/fuck/mc/Texts/female/pussy_fisting.js
@@ -0,0 +1,11 @@
+setup.actionText ??= {};
+setup.actionText.pussy_fisting = {
+    texts: [
+        "Your ram your fist deep into %name%'s pussy, you feel her vagina pulsing around you!",
+        "You stuff %name%'s pussy with your hand, watching her squirm with pleasure.",
+        "%name%'s body tenses with pleasure as you fist her pussy.",
+        "You explore %name%'s body with your fist, fucking her with all five fingers inside her vagina.",
+        "%name% moans with pleasure as you stuff her pussy, slowly inserting all your fingers into her tight vagina.",
+        "You stick your hand inside %name% tight pussy."
+    ]
+}
