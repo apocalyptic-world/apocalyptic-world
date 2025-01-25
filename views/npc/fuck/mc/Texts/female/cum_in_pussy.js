19db1d936015b700da2849591d0fd580e1711e77
Illusion-XIII <illusion-xiii@hotmail.com>
2025-01-25 14:01:50 -0500

    Update cum_in_pussy.js (#207)

--- views/npc/fuck/mc/Texts/female/cum_in_pussy.js
+++ views/npc/fuck/mc/Texts/female/cum_in_pussy.js
@@ -2,5 +2,9 @@ setup.actionText ??= {};
 setup.actionText.cum_in_pussy = {
     texts: [
         "You continue to fuck %name% non-stop and you pump her pussy full of your seed, filling her up with your white, warm cum."
+        "You shudder as your seed spills deep within %name%, triggering sensations that make her toes curl and heart race."
+        "You shove yourself as deep inside %name% as you can, your hips bucking wildly as your climax overwhelms you. %name% feels the heat of your release pulsing against her inner walls, making it impossible not to clench around you in response."
+        "As you finally climax inside %name%, your hot seed fills her depths and sends shockwaves through every fiber of her being. You growl in pleasure, thrusting one last time as if to ensure that every drop finds its way home."
+        "You groan with pleasure, your body shuddering as wave after wave of release washes over you.  As you cum inside %name%, her body tenses and shudders around your thrusting member, feeling your warmth spreading through her."
     ]
 }
