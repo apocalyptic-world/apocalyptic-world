6c752089da3b6a18d93edf0bdc916f48bacb357c
decker-666 <144735638+decker-666@users.noreply.github.com>
2025-06-26 23:11:02 +0200

    mft text uniformization (#248)

--- views/npc/fuck/npc/Texts/male_on_female_and_transfemale/male_on_female_and_transfemale_texts.js
+++ views/npc/fuck/npc/Texts/male_on_female_and_transfemale/male_on_female_and_transfemale_texts.js
@@ -1,150 +1,150 @@
 setup.actionText ??= {};
 setup.actionText.male_on_female_and_transfemale = {
-    threesome_t_anal: [
+    anal: [
         "%female% moans softly as %male% fucks her ass."
     ],
-    threesome_t_anal_b_bj: [
+    anal_b_bj: [
         "%male% fucks %female% in the ass, while she sucks %transfemale%'s dick!"
     ],
-    threesome_t_anal_b_handjob: [
+    anal_b_handjob: [
         "%male% fucks %female% in the ass, while she gives %transfemale% a handjob!"
     ],
-    threesome_t_anal_b_pussy: [
+    anal_b_pussy: [
         "%male% fucks %female% in the ass, while %transfemale% fucks her pussy!"
     ],
-    threesome_t_anal_fingering: [
+    anal_fingering: [
         "%male%'s fingers dig deep into %female%'s tight asshole!"
     ],
-    threesome_t_anal_reverse: [
+    anal_reverse: [
         "%male% moans softly as %female% fucks his ass with a strapon."
     ],
-    threesome_t_anal_reverse_cum: [
+    anal_reverse_cum: [
         "%male% starts to cum, while %female%'s strapon is still in his ass!"
     ],
-    threesome_t_bj: [
+    bj: [
         "%female% and %transfemale% suck %male%'s hard dick in turn with their warm mounths and agile tongues!"
     ], 
-    threesome_t_bj_anal_reverse: [
+    bj_anal_reverse: [
         "%female% gives %male% a blowjob, while %transfemale% fucks his ass."
     ],
-    threesome_t_bj_a_anal: [
+    bj_a_anal: [
         "%female% sucks %male%'s cock, while she fucks %transfemale% in the ass with her strapon!"
     ],
-    threesome_t_bj_b_anal: [
+    bj_b_anal: [
         "%female% sucks %male%'s cock, while %transfemale% fucks her in the ass!"
     ],
-    threesome_t_bj_b_pussy: [
+    bj_b_pussy: [
         "%female% sucks %male%'s cock, while %transfemale% fucks her pussy!"
     ],
-	threesome_t_bj_bj_reverse: [
+	bj_bj_reverse: [
         "%female% sucks %male%'s cock, while %male% sucks %transfemale%'s."
     ], 
-    threesome_t_bj_reverse: [
+    bj_reverse: [
         "%male% is sucking %female%'s strapon."
     ], 
-    threesome_t_bj_reverse_anal_reverse: [
+    bj_reverse_anal_reverse: [
         "%female% fucks %male%'s mouth, while %transfemale% fucks his ass."
     ],
-    threesome_t_cum_in_ass: [
+    cum_in_ass: [
         "With a groan, %male% cums in %female%'s ass."
     ],
-    threesome_t_cum_in_mouth: [
+    cum_in_mouth: [
         "%male% fills %female% and %transfemale%'s mouths with his cum."
     ],
-    threesome_t_cum_in_pussy: [
+    cum_in_pussy: [
         "%male% continues to fuck %female% non-stop and pump her pussy full of his seed."
     ],
-    threesome_t_cum_on_ass: [
+    cum_on_ass: [
         "With a groan, %male% cums on %female%'s ass."
     ],
-    threesome_t_cum_on_back: [
+    cum_on_back: [
         "With a groan, %male% cums on %female% and %transfemale%'s back sides."
     ],
-    threesome_t_cum_on_face: [
+    cum_on_face: [
         "%male% cums all over %female% and %transfemale%'s faces."
     ],
-    threesome_t_cum_on_stomach: [
+    cum_on_stomach: [
         "%male% finishes on %female% and %transfemale%'s stomachs."
     ],
-    threesome_t_cum_on_tits: [
+    cum_on_tits: [
         "%male%'s cum covers %female% and %transfemale%'s breasts."
     ],
-    threesome_t_cum_throat: [
+    cum_throat: [
         "%male% grabs %female%'s head and holds her as he thrusts his throbbing cock deep into her throat."
     ],
-    threesome_t_dp: [
+    dp: [
         "%male% takes turns grabbing %female% and %transfemale%'s heads, driving his hard dick deep into their throats."
     ],
-    threesome_t_footjob: [
+    footjob: [
         "%male% pleasure himself with %female% and %transfemale%'s feet."
     ],
-    threesome_t_footjob_cum: [
+    footjob_cum: [
         "%male% covers %female% and %transfemale%'s feet with his seed."
     ],    
-    threesome_t_handjob: [
+    handjob: [
         "%female% and %transfemale% give %male% a nice handjob, a good start!"
     ],
-	threesome_t_handjob_a_anal: [
+	handjob_a_anal: [
         "%female% gives %male% a nice handjob, while fucking %transfemale%'s ass with her strapon."
     ],
-    threesome_t_handjob_anal_reverse: [
+    handjob_anal_reverse: [
         "%female% gives %male% a handjob, while %transfemale% fucks his ass."
     ],
-    threesome_t_handjob_b_bj: [
+    handjob_b_bj: [
         "%female% gives %male% a nice handjob, while sucking %transfemale%'s cock."
     ],
-    threesome_t_handjob_cum: [
+    handjob_cum: [
         "%male% jerks off with %female% and %transfemale%'s hands tightly gripped around his cock."
     ],
-    threesome_t_handjob_b_pussy: [
+    handjob_b_pussy: [
         "%female% gives %male% a nice handjob, while %transfemale% fucks her pussy."
     ],
-    threesome_t_kissing: [
+    kissing: [
         "%female% and %transfemale% are kissing %male% and each other sweetly."
     ],
-    threesome_t_nipple_lick: [
+    nipple_lick: [
         "%male% and %transfemale% lick %female%'s nipples, and squeeze her tits hard!"
     ],
-    threesome_t_nipple_lick_anal_reverse: [
+    nipple_lick_anal_reverse: [
         "%male% licks %female%'s nipple, while %transfemale% fucks his ass."
     ],
-    threesome_t_pussy: [
+    pussy: [
         "%male% fucks %female%'s tight pussy"
     ],
-    threesome_t_pussy_a_sn: [
+    pussy_a_sn: [
         "%male% fucks %female%, while she and %transfemale% lick each other!"
     ],
-    threesome_t_pussy_anal_reverse: [
+    pussy_anal_reverse: [
         "%male% fucks %female%'s pussy, while %transfemale% fucks his ass."
     ],
-    threesome_t_pussy_b_anal: [
+    pussy_b_anal: [
         "%male% fucks %female%, while %transfemale% fucks her in the ass!"
     ],
-	threesome_t_pussy_b_bj: [
+	pussy_b_bj: [
         "%male% fucks %female%, while she gives %transfemale% a blowjob!"
     ],
-	threesome_t_pussy_bj_reverse: [
+	pussy_bj_reverse: [
         "%male% fucks %female%, while giving %transfemale% a blowjob!"
     ],
-    threesome_t_pussy_fingering: [
+    pussy_fingering: [
         "%male% fingers %female%'s pussy, she is getting very wet!"
     ],
-    threesome_t_pussy_lick: [
+    pussy_lick: [
         "%male% licks %female%'s clit, she seems to like it a lot!"
     ],
-    threesome_t_pussy_lick_anal_reverse: [
+    pussy_lick_anal_reverse: [
         "%male% licks %female%'s pussy, while %transfemale% fucks his ass."
     ],
-	threesome_t_pussy_lick_b_bj: [
+	pussy_lick_b_bj: [
         "%male% licks %female%'s pussy, while she gives %transfemale% a blowjob."
     ],
-    threesome_t_sn: [
+    sn: [
         "%male% licks %female%'s pussy, while she sucks %transfemale%'s dick, who is sucking %male%'s!"
     ],
-    threesome_t_sn_anal_reverse: [
+    sn_anal_reverse: [
         "%male% licks %female%'s pussy, while she gives %male% a blowjob and %transfemale% fucks his ass."
     ],
-    threesome_t_titjob: [
+    titjob: [
         "%female% and %transfemale% give %male% a nice titjob!"
     ]
 };
