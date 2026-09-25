6b3a74148e74864013201e066f09f44a1945747d
AW <marislazda@gmail.com>
2026-09-25 16:35:27 +0300

    Updates

--- js/sex.js
+++ js/sex.js
@@ -16,5 +16,24 @@ setup.sex = {
         }
     
         return parseInt(hornyPerStep);
+    },
+
+    /**
+     * Mirror of calcHornyIncrease for the partner: the more sex the MC has had
+     * ($player.sexp), the faster partners get aroused. Asymptotic, so it tops
+     * out at +60% and a long save can't trivialise every scene.
+     */
+    calcNpcHornyIncrease: function(npc, hornyGive, sexAction)
+    {
+        if (!hornyGive) {
+            return hornyGive;
+        }
+        return Math.max(1, Math.round(hornyGive * setup.sex.sexpMultiplier()));
+    },
+
+    sexpMultiplier: function()
+    {
+        const sexp = Math.max(0, variables().player.sexp ?? 0);
+        return 1 + 0.6 * (sexp / (sexp + 150));
     }
 }
